16fbe1c5825e36dd9e958b50665cd1044105bc62
Matt Gibson <gibson.matthew@hotmail.com>
2026-02-28 13:29:27 -0500

    Initial Copy

---
+++ enumapoke/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json
+;
+
+HttpClient client = new HttpClient();
+
+int userId = 1;
+
+
+
+Console.WriteLine("Hello, and Welcome to the World of Pokemon");
+Console.WriteLine("Enter a Id Number between 1 and 151 to Identify one of the OGs!!!");
+
+userId = int.Parse(Console.ReadLine());
+
+Console.WriteLine();
+
+string apiString = "https://pokeapi.co/api/v2/pokemon/" + userId + "/";
+
+string jsonResponse = client.GetStringAsync(apiString).Result;
+
+Pokemon pokemon = JsonSerializer.Deserialize<Pokemon>(jsonResponse);
+
+string name = pokemon.name;
+
+Console.WriteLine("The Pokemon with ID " + userId + " is " + name.ToUpper());
+
+
+
+
+public class Pokemon
+{
+    public string name { get; set; }
+}
